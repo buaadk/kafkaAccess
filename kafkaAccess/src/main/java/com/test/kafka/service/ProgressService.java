4714f2e8c63bec126ef40cbb56cece21f369785e
buaadk <buaadk@foxmail.com>
2018-04-16 18:19:23 +0800

    delete

--- kafkaAccess/src/main/java/com/test/kafka/service/ProgressService.java
+++
@@ -1,9 +0,0 @@
-package com.test.kafka.service;
-
-import com.test.kafka.vo.Progress;
-
-public interface ProgressService {
-	public void save(Progress progress);
-
-	public Progress queryByTableName(String tableName);
-}
